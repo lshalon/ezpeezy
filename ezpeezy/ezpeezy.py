070aea5597995f8cd8b8a1c1eea16da38490262c
Liam Shalon <shalon.liam@gmail.com>
2020-03-29 14:11:35 -0700

    more customization

--- ezpeezy/ezpeezy.py
+++ ezpeezy/ezpeezy.py
@@ -3,14 +3,16 @@ from tensorforce.agents import DeepQNetwork
 from .environment import CustomEnvironment
 
 class Ezpeezy():
-    def __init__(self, model_fn):
-        env = CustomEnvironment({'dropout_rate': '0:0.9', 'max_pool_size': 'i2:5'}, input_model=model_fn, opt='min')
+    def __init__(self, config, model_fn, opt='max'):
+        env = CustomEnvironment(config, input_model=model_fn, opt=opt)
         agent = DeepQNetwork(states=env.states(), actions=env.actions(),
                      max_episode_timesteps=env.max_episode_timesteps(),
                      memory=60, batch_size=1, exploration=dict(type='decaying', unit='timesteps', decay='exponential',
                                                             initial_value=0.9, decay_steps=1000, decay_rate=0.8)
                      )
 
-        runner = Runner(agent=agent, environment=env)
-        runner.run(num_episodes=100)
-        runner.close()
+        self.runner = Runner(agent=agent, environment=env)
+
+    def run(self, num_episodes):
+        self.runner.run(num_episodes=100)
+        self.runner.close()
