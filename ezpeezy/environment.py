5abbd957df9d1edecfd06d10ef1d391c36c0c304
Liam Shalon <shalon.liam@gmail.com>
2020-03-29 16:45:20 -0700

    -

--- ezpeezy/environment.py
+++ ezpeezy/environment.py
@@ -1,49 +1,15 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
 from .hyperparameter import HyperparameterSettings
 from .data import DataManager
+
 from tensorforce.environments import Environment
 
 import random
 import numpy as np
 import math
-
-num_classes = 10
-
-# input image dimensions
-img_rows, img_cols = 28, 28
-
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
-
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-indexes_to_use_for_training = random.sample(range(len(x_train)), int(len(x_train) / 2))
 
 class CustomEnvironment(Environment):
   
@@ -115,7 +81,7 @@ class CustomEnvironment(Environment):
       history = self._internal_model.fit(X_train, y_train,
             batch_size=512,
             epochs=75,
-            verbose=0,
+            verbose=1,
             validation_data=(X_valid, y_valid))
       
       each_reward.append(-min(history.history['val_loss']) if self._opt == 'min' else max(history.history['val_loss']))
