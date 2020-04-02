8173ebe5b81d488e651e0ceffc3d38f7dfb4593d
Liam Shalon <shalon.liam@gmail.com>
2020-04-02 10:53:46 -0700

    change setup name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ezpeezy-lshalon", # Replace with your own username
+    name="ezpeezy", # Replace with your own username
     version="0.0.2",
     author="Example Author",
     author_email="author@example.com",
